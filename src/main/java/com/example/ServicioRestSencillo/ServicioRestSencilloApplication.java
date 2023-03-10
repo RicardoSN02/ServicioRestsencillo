5a18081a1c7e4e2b02691dd6c854ebbd209adf25
Ricardo Saavedra <rjsaavedran@gmail.com>
2023-03-10 13:57:46 -0700

    commit con aplicacion primer commit

---
+++ src/main/java/com/example/ServicioRestSencillo/ServicioRestSencilloApplication.java
@@ -0,0 +1,13 @@
+package com.example.ServicioRestSencillo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioRestSencilloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioRestSencilloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ServicioRestSencillo/ServicioRestSencilloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ServicioRestSencillo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioRestSencilloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
